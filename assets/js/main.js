96030ef6f571d264b1b2fde148449e1140f8b654
bhatnagardivy <bhatnagar.divy@outlook.com>
2024-05-10 16:42:41 +0530

    Last screen completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,35 @@
 $(document).ready(() => {
 
-  // To open the popup box
-
+  // On click of popup button
   $("#popupButton").on("click", () => {
     $("#popup").css("display", "block");
-
+    $("#firstScreen").css("display", "block");
+    $("#secondScreen").css("display", "none");
+    $("#lastScreen").css("display", "none");
   });
 
-  // To close the popup box
-  $("#closeButton").on("click", () => {
-    $("#popup").css("display", "none");
-    $("#popupButton").prop("disabled", false);
-  });
-
+  // On click of upload button
   $("#uploadDesign").on("click", () => {
+    $("#popup").css("display", "block");
     $("#firstScreen").css("display", "none");
     $("#secondScreen").css("display", "block");
+    $("#lastScreen").css("display", "none");
   });
 
+  // On click of close button or back button 
+  $("#closeButton, #backButton").on("click", () => {
+    $("#popup").css("display", "block");
+    $("#firstScreen").css("display", "none");
+    $("#secondScreen").css("display", "none");
+    $("#lastScreen").css("display", "block");
+  });
+
+  // On click of proceed button
+  $("#proceedButton").on("click", () => {
+    $("#popup").css("display", "none");
+    $("#firstScreen").css("display", "none");
+    $("#secondScreen").css("display", "none");
+    $("#lastScreen").css("display", "none");
+  })
 });
 
