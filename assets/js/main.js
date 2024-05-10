faaf632ccd037dc894e0ee699370208947362d33
bhatnagardivy <bhatnagar.divy@outlook.com>
2024-05-10 15:02:48 +0530

    First screen completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,18 @@ $(document).ready(() => {
 
   $("#popupButton").on("click", () => {
     $("#popup").css("display", "block");
+
   });
 
   // To close the popup box
   $("#closeButton").on("click", () => {
     $("#popup").css("display", "none");
+    $("#popupButton").prop("disabled", false);
+  });
+
+  $("#uploadDesign").on("click", () => {
+    $("#firstScreen").css("display", "none");
+    $("#secondScreen").css("display", "block");
   });
 
 });
